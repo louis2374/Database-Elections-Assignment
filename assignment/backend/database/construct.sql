06b0e5162f3fdfd352cf46428c285b845e46090e
louis2374 <115995518+louis2374@users.noreply.github.com>
2024-01-15 01:23:38 +0000

    improve construction scripts

--- assignment/backend/database/construct.sql
+++ assignment/backend/database/construct.sql
@@ -1,37 +1,39 @@
---Party table
+--all PKs use serial to auto generate, and the majority of names are unique (will cause conflict on duplicate)
+
+--party table
 CREATE TABLE tbl_parties (
-    party_id INT PRIMARY KEY,
-    party_name VARCHAR(64)
+    party_id SERIAL PRIMARY KEY,
+    party_name VARCHAR(64) UNIQUE
 );
 
 --counties table
 CREATE TABLE tbl_counties (
-    county_id INT PRIMARY KEY,
-    county_name VARCHAR(64)
+    county_id SERIAL PRIMARY KEY,
+    county_name VARCHAR(64) UNIQUE
 );
 
 --regions table
 CREATE TABLE tbl_regions (
-    region_id INT PRIMARY KEY,
-    region_name VARCHAR(64)
+    region_id SERIAL PRIMARY KEY,
+    region_name VARCHAR(64) UNIQUE
 );
 
 --countries table
 CREATE TABLE tbl_countries (
-    country_id INT PRIMARY KEY,
-    country_name VARCHAR(64)
+    country_id SERIAL PRIMARY KEY,
+    country_name VARCHAR(64) UNIQUE
 );
 
 --constituency types table
 CREATE TABLE tbl_constituency_types (
-    type_id INT PRIMARY KEY,
-    type_name VARCHAR(64)
+    type_id SERIAL PRIMARY KEY,
+    type_name VARCHAR(64) UNIQUE
 );
 
 --constituencies table
 CREATE TABLE tbl_constituencies (
-    constituency_id INT PRIMARY KEY,
-    constituency_name VARCHAR(64),
+    constituency_id SERIAL PRIMARY KEY,
+    constituency_name VARCHAR(64) UNIQUE,
     constituency_type INT,
 
     --foreign keys
@@ -40,9 +42,10 @@ CREATE TABLE tbl_constituencies (
 
 --candidates table, holds compiled information about a candidate
 CREATE TABLE tbl_candidates (
-    candidate_id INT PRIMARY KEY,
+    candidate_id SERIAL PRIMARY KEY,
     candidate_forename VARCHAR(64),
     candidate_surname VARCHAR(64),
+    candidate_gender VARCHAR(16),
     candidate_sitting_mp BOOLEAN,
     party_id INT,
     county_id INT,
@@ -60,7 +63,7 @@ CREATE TABLE tbl_candidates (
 
 --votes table, stores vote information about each candidate
 CREATE TABLE tbl_votes (
-    vote_id INT PRIMARY KEY,
+    vote_id SERIAL PRIMARY KEY,
     vote_votes INT,
     candidate_id INT,
 
@@ -70,13 +73,13 @@ CREATE TABLE tbl_votes (
 
 --systems table
 CREATE TABLE tbl_systems (
-    system_id INT PRIMARY KEY,
+    system_id SERIAL PRIMARY KEY,
     system_name VARCHAR(128)
 );
 
 --system results table
 CREATE TABLE tbl_results (
-    result_id INT PRIMARY KEY,
+    result_id SERIAL PRIMARY KEY,
     system_id INT,
     result_seats INT,
     result_popular_votes_vs_seat_percent FLOAT,
@@ -91,3 +94,83 @@ CREATE TABLE tbl_results (
     FOREIGN KEY (winning_party_id) REFERENCES tbl_parties(party_id),
     FOREIGN KEY (result_most_seats_party) REFERENCES tbl_parties(party_id)
 );
+
+
+--insertion function, will ensure all required information exists for candidate, then insert the candidate
+CREATE OR REPLACE FUNCTION insert_candidate(
+    --just pass in all the required params
+    v_forename VARCHAR(64),
+    v_surname VARCHAR(64),
+    v_gender VARCHAR(16),
+    v_sitting_mp BOOLEAN,
+    v_party_name VARCHAR(64),
+    v_county_name VARCHAR(64),
+    v_region_name VARCHAR(64),
+    v_country_name VARCHAR(64),
+    v_constituency_name VARCHAR(64),
+    v_constituency_type VARCHAR(64),
+    v_votes INT
+) RETURNS VOID AS
+
+--function is defined between these two sets of $$
+$$
+DECLARE
+--stores the id of the inserted candidate for use when adding the vote data, saves a query
+    v_candidate_id INT;
+
+--group all statements
+BEGIN
+    --insert a type, duplicates are silently ignored
+    INSERT INTO tbl_constituency_types (type_name) VALUES (v_constituency_type) ON CONFLICT DO NOTHING;
+
+    --insert a constituency, duplicates are silently ignored
+    INSERT INTO tbl_constituencies (constituency_name, constituency_type)
+    VALUES (v_constituency_name, (SELECT type_id FROM tbl_constituency_types WHERE type_name = v_constituency_type))
+    ON CONFLICT DO NOTHING;
+
+    -- insert a county, ignores duplicates
+    INSERT INTO tbl_counties (county_name) VALUES (v_county_name) ON CONFLICT DO NOTHING;
+
+    --insert region
+    INSERT INTO tbl_regions (region_name) VALUES (v_region_name) ON CONFLICT DO NOTHING;
+
+    --insert a country
+    INSERT INTO tbl_countries (country_name) VALUES (v_country_name) ON CONFLICT DO NOTHING;
+
+    --insert a party and do nothing if it exists
+    INSERT INTO tbl_parties (party_name) VALUES (v_party_name) ON CONFLICT DO NOTHING;
+
+    --create a candidate using all this new data
+    --does a query for the ids of the foreign keys
+    INSERT INTO tbl_candidates(
+        candidate_forename,
+        candidate_surname,
+        candidate_gender,
+        candidate_sitting_mp,
+        party_id,
+        county_id,
+        region_id,
+        country_id,
+        constituency_id
+    ) VALUES (
+        v_forename,
+        v_surname,
+        v_gender,
+        v_sitting_mp,
+        (SELECT party_id FROM tbl_parties WHERE party_name = v_party_name),
+        (SELECT county_id FROM tbl_counties WHERE county_name = v_county_name),
+        (SELECT region_id FROM tbl_regions WHERE region_name = v_region_name),
+        (SELECT country_id FROM tbl_countries WHERE country_name = v_country_name),
+        (SELECT constituency_id FROM tbl_constituencies WHERE constituency_name = v_constituency_name)
+        --returns the new id and saves into candidate_id var
+    ) RETURNING candidate_id INTO v_candidate_id;
+
+    --add vote data, using saved id
+    INSERT INTO tbl_votes (vote_votes, candidate_id) VALUES (v_votes, v_candidate_id);
+--end of group
+END;
+--end function code definition
+$$
+
+--i have to specify the lanuage otherwise it gives no lang specified error
+LANGUAGE plpgsql;
